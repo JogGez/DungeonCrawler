cbf21bcdfce27e0453520f6c691ae65cf6ab8f26
JogGez <JogGez@DESKTOP-QGOTUAH>
2017-09-24 02:16:10 +0200

    new Commit

---
+++ src/worldofzuul/JavaProjects.java
@@ -0,0 +1,14 @@
+package worldofzuul;
+
+
+public class JavaProjects 
+{
+
+    public static void main(String[] args) 
+    {
+        Game game = new Game();
+        game.play();
+        
+    }
+
+}
