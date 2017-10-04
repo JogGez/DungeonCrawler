c06b6d0cba1e572368b6948fe2c4bdad2860a2eb
Jonathan <>
2017-10-04 18:50:19 +0200

    Leget lidt med monster og player class, du burde nu kunne drabe et monster. Har ikke kunne få sat den ind i et room endnu.

--- src/worldofzuul/Monster.java
+++ src/worldofzuul/Monster.java
@@ -15,37 +15,84 @@ public class Monster {
     private int health;
     private int power;
     private String description;
+    private boolean isDead = false;
 
-    public String getName() {
+    public String getName() 
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) 
+    {
         this.name = name;
     }
 
-    public int getHealth() {
+    public int getHealth() 
+    {
         return health;
     }
 
-    public void setHealth(int health) {
+    public void setHealth(int health) 
+    {
         this.health = health;
     }
+    
+    public int health()
+    {
+        int hp = 150;
+        return hp;
+    }
 
-    public int getPower() {
+    public int getPower() 
+    {
         return power;
     }
 
-    public void setPower(int power) {
+    public void setPower(int power) 
+    {
         this.power = power;
     }
 
-    public String getDescription() {
+    public String getDescription() 
+    {
         return description;
     }
 
-    public void setDescription(String description) {
+    public void setDescription(String description) 
+    {
         this.description = description;
+    }
+    
+    
+        public double power(Player pc)
+    {
+        // Calculate damage dealt.
+        //Should most likly be put under default weapons (Hands/Swords/Mace etc.)
+        // Manipulation of the damage
+        // Makes the damage be a bit more random
+        double damage = Math.round((Math.random() * (15 - 5)));
+        pc.takeDamage(damage);
+        return damage;
+    }
+        public void takeDamage(double damage)
+    {
+        //Checks damage taken, if 0 or less, return isDead
+        if (health - damage <= 0)
+        {
+        health = 0;
+        isDead = true;
+        }
+        else
+        {
+            // Otherwise minus health with damage taken
+            health -= damage;
+        }
+    }
+    
+    // State of death
+    public boolean isDead()
+    {
+        return isDead;
     }
     
     
