23b30dd4ca394280b0236abac05b7d12d5be9b27
frederiktt <32263257+frederiktt@users.noreply.github.com>
2017-10-05 12:48:54 +0200

    Ændret Welcome message til "Welcome to Dungeon Crawler"

--- src/worldofzuul/Game.java
+++ src/worldofzuul/Game.java
@@ -112,7 +112,7 @@ public class Game
     private void printWelcome()
     {
         System.out.println();
-        System.out.println("Welcome to the World of Zuul!");
+        System.out.println("Welcome to Dungeon Crawler!");
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
         System.out.println("Type '" + CommandWord.HELP + "' if you need help.");
         System.out.println();
