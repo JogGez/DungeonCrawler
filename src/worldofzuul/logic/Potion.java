613d8f2bacaacf3d9d790dfc97d3c6551a6c56b3
Linea <Linea.hoffmann.christophersen@gmail.com>
2017-10-27 10:46:53 +0200

    Kommentare til Potion, og default helper i helper class

--- src/worldofzuul/logic/Potion.java
+++ src/worldofzuul/logic/Potion.java
@@ -1,10 +1,22 @@
 package worldofzuul.logic;
 
+/**
+ * Inherit from the Item class
+ * @author 
+ */
 public class Potion extends Item
 {
-
+    //Int variable healthRecovery 
     private int healthRecovery;
 
+    
+    /**
+     * 
+     * @param name
+     * @param description
+     * @param ascii
+     * @param healthRecovery 
+     */
     public Potion(String name, String description, String ascii, int healthRecovery)
     {
 
@@ -14,7 +26,10 @@ public class Potion extends Item
         this.healthRecovery = healthRecovery;
     }
 
-
+    /**
+     * 
+     * @return int
+     */
     public int getHealthRecovery()
     {
         return healthRecovery;
