4e0d64a95ec0c0aae7e13c223961ce85d1f69f52
Linea <Linea.hoffmann.christophersen@gmail.com>
2017-11-07 12:40:49 +0100

    Tilføjet getter og setter for alle datafelterne

--- src/worldofzuul/logic/Item.java
+++ src/worldofzuul/logic/Item.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package worldofzuul.logic;
 
 /**
@@ -14,4 +9,36 @@ public class Item
     public String name;
     public String description;
     public String ascii;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public String getAscii()
+    {
+        return ascii;
+    }
+
+    public void setAscii(String ascii)
+    {
+        this.ascii = ascii;
+    }
+    
+    
 }
